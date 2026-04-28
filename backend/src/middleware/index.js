81b6f1fe6b341c01436d63748ca2b7697b95ea2b
Free-codaer <nabilahsally25@gmail.com>
2026-04-28 03:39:09 +0200

    Fixed Issue #115

--- backend/src/middleware/index.js
+++ backend/src/middleware/index.js
@@ -1,11 +1,13 @@
 const logger = require('./logger');
 const errorHandler = require('./errorHandler');
+const { requestContextMiddleware } = require('./requestContext');
 const authMiddleware = require('./authMiddleware');
 const { smartRateLimiter, applyRateLimit } = require('./rateLimiter');
 
 module.exports = {
   logger,
   errorHandler,
+  requestContextMiddleware,
   authMiddleware,
   smartRateLimiter,
   applyRateLimit,
